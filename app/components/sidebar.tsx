d915cc36bedb31a942d903c561792520ef46b8c4
xin <xinn314@gmail.com>
2023-08-02 12:37:26 +0800

    rename

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,7 +117,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT
+          AssistBot
         </div>
         <div className={styles["sidebar-sub-title"]}>
           本项目为个人学习研究使用，请勿分享给他人
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Bot",
-  description: "Your personal ChatGPT Bot.",
+  title: "AssistBot",
+  description: "Your personal AssistBot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Bot",
+    title: "AssistBot",
     statusBarStyle: "default",
   },
 };
